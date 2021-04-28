9b768d4bdfd179a397c355b0c54a0f684947aa75
Peter <griffitp12@gmail.com>
2021-04-29 08:35:47 +0900

    knexfile created and written

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./data",
+    },
+  },
+
+  staging: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./data",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./data",
+    },
+  },
+};
