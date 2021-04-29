f26ad768a3e7df5bd0db2ff3312e7fdefa09de3f
Ken Kurosaki <ken.kurosaki1991@gmail.com>
2021-04-29 20:24:39 +0900

    Created basic components. Working on FilterByState

---
+++ src/components/FilterByState.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      Locations:
+      <select v-model="state" id="state-input" />
+        <option v-for="state of locations.state" v-bind:key="state"> 
+        </option>
+      <input type="text" id="city-input" placeholder="Enter City" />
+      <input type="text" id="highway-input" placeholder="Enter Highway" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterByState",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
