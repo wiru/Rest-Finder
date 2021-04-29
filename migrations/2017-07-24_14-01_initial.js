bd002168b95c2317e441cb7dc8633605e3ac618a
Alix Fachin <alix.fachin@me.com>
2021-04-29 10:39:48 +0900

    Migration and seeding complete

--- migrations/2017-07-24_14-01_initial.js
+++ migrations/2017-07-24_14-01_initial.js
@@ -10,4 +10,6 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("locations");
+};
