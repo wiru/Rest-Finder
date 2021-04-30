11654ebc42f31b54181a593b344263345207ed1e
Peter <griffitp12@gmail.com>
2021-04-30 11:17:11 +0900

    database updates with more location data

---
+++ migrations/20210430104315_add_facility_subtytpe.js
@@ -0,0 +1,12 @@
+
+exports.up = function (knex) {
+    return knex.schema.table('locations', function (table) {
+        table.text("subtype")
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.table('locations', function (table) {
+        table.dropColumn("subtype")
+    })
+};
